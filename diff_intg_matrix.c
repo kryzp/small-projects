b8db0e7ae2a3f2ceb8567d415704eeb52ff8577e
kryzp <kprzeczek4@gmail.com>
2023-01-08 19:24:35 +0000

    Add files via upload

---
+++ diff_intg_matrix.c
@@ -0,0 +1,39 @@
+
+/*
+ * Does polynomial differentiation and integration through use of
+ * an infinate matrix. Remember, integration here is literally the
+ * just multiplying by the inverse differentiation matrix!
+ */
+
+#include <stdio.h>
+
+void differentiate(double* poly, double* out, int len) {
+    for (int i = 0; i < len-1; i++) {
+        out[i] += poly[i+1] * (i+1);
+    }
+}
+
+void integrate(double* poly, double* out, int len) {
+    for (int i = 0; i < len+1; i++) {
+        out[i] += poly[i-1] * (1.0 / (double)(i));
+    }
+    out[0] = 0.0;
+}
+
+int main(int argc, char** argv) {
+    const unsigned SIZE = 4;
+
+    double poly[] = { 1.0, 1.0, 1.0, 0.0 };
+    double diffd[SIZE] = {0};
+    double intgd[SIZE] = {0};
+
+    differentiate(poly, diffd, SIZE);
+    integrate(poly, intgd, SIZE);
+
+    for (int i = 0; i < SIZE; i++) {
+        printf("%.2f x^%d | d/dx=%.2f x^%d, int=%.2f x^%d\n", poly[i], i, diffd[i], i, intgd[i], i);
+    }
+
+    return 0;
+}
+
